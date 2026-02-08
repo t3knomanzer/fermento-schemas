5ba1843980f916a2ba6bb5489f42984e87356d0d
t3knomanzer <rhenares0@gmail.com>
2026-02-08 23:15:58 +0100

    Simplifying embedded schemas

--- fermento-embedded-schemas/fermento_embedded_schemas/feeding_event.py
+++ fermento-embedded-schemas/fermento_embedded_schemas/feeding_event.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-from fermento_embedded_schemas.base import BaseSchema
+from base import BaseSchema
 
 
 class FeedingEventSchema(BaseSchema):
@@ -7,30 +6,16 @@ class FeedingEventSchema(BaseSchema):
     Schema for telemetry data validation.
     """
 
-    schema: tuple[int, int, int]
-    device_id: str
-    message_id: str
-    timestamp: str
-    date: datetime
-    starter_id: int
-    starter_ratio: float
-    water_ratio: float
-    flour_ratio: float
-    flour_blend_id: int
-    jar_id: int
+    id: int
+    date: str
+    starter: dict
+    jar: dict
 
     @classmethod
     def _get_fields(cls) -> list[str]:
         return [
-            "schema",
-            "device_id",
-            "message_id",
-            "timestamp",
+            "id",
             "date",
-            "starter_id",
-            "starter_ratio",
-            "water_ratio",
-            "flour_ratio",
-            "flour_blend_id",
-            "jar_id",
+            "starter",
+            "jar",
         ]
--- fermento-embedded-schemas/fermento_embedded_schemas/feeding_sample.py
+++ fermento-embedded-schemas/fermento_embedded_schemas/feeding_sample.py
@@ -6,10 +6,6 @@ class FeedingSampleSchema(BaseSchema):
     Schema for telemetry data validation.
     """
 
-    schema: tuple[int, int, int]
-    device_id: str
-    message_id: str
-    timestamp: str
     feeding_event_id: int
     temperature: float
     humidity: float
@@ -19,10 +15,6 @@ class FeedingSampleSchema(BaseSchema):
     @classmethod
     def _get_fields(cls) -> list[str]:
         return [
-            "schema",
-            "device_id",
-            "message_id",
-            "timestamp",
             "feeding_event_id",
             "temperature",
             "humidity",
